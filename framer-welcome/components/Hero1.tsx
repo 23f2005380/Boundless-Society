acb310211e55809300968a5162e137a1de974a4a
rahul <rahulbarakoti4@gmail.com>
2025-07-03 12:46:07 +0530

    Main button added

--- framer-welcome/components/Hero1.tsx
+++ framer-welcome/components/Hero1.tsx
@@ -1,30 +1,27 @@
 import React from "react";
-import VideoContainer from "@/components/videoCont"
+import VideoContainer from "@/components/videoCont";
+import MainButton from "./MainButton";
 
 export default function Hero() {
-    return (
-        <section className="px-4 md:px-6 sm:h-[150vh] overflow-hidden">
-        <div className="text-center mb-8">
-          <h1 className="text-4xl md:text-6xl lg:text-8xl font-black text-[#3B001B] mb-4 md:mb-6 relative">
-            BOUNDLESS
-          </h1>
-          <div className="p-3 md:p-4 rounded-lg max-w-xs md:max-w-2xl mx-auto mb-6 md:mb-8">
-            <h2 className="text-lg md:text-2xl lg:text-3xl font-bold text-[#3B001B] mb-3 md:mb-4">
-              IIT 'M BS TRAVEL SOCIETY
-            </h2>
-            <div>
-             
-            <button className="bg-[#FFE878] hover:translate-y-1 text-[#3B001B] transition:translate-y px-6 md:px-8 py-2 rounded-full font-semibold text-sm md:text-base" style={{boxShadow : "0px 5px 1px #9c1352"}}>
-              Join Us
-            </button>
-            
-            </div>
+  return (
+    <section className="px-4 md:px-6 sm:h-[150vh] overflow-hidden">
+      <div className="text-center mb-8">
+        <h1 className="text-4xl md:text-6xl lg:text-8xl font-black text-[#3B001B] mb-4 md:mb-6 relative">
+          BOUNDLESS
+        </h1>
+        <div className="p-3 md:p-4 rounded-lg max-w-xs md:max-w-2xl mx-auto mb-6 md:mb-8">
+          <h2 className="text-lg md:text-2xl lg:text-3xl font-bold text-[#3B001B] mb-3 md:mb-4">
+            IIT 'M BS TRAVEL SOCIETY
+          </h2>
+          <div>
+            <MainButton onClick={() => {}}>Join Us</MainButton>
           </div>
         </div>
-        {/* Decorative Circle */}
-        <div className="flex justify-center mb-8 md:mb-12">
-          <VideoContainer />
-        </div>
-      </section>
-    )
+      </div>
+      {/* Decorative Circle */}
+      <div className="flex justify-center mb-8 md:mb-12">
+        <VideoContainer />
+      </div>
+    </section>
+  );
 }
--- framer-welcome/components/Prev.jsx
+++ framer-welcome/components/Prev.jsx
@@ -1,9 +1,12 @@
 import React from "react";
 import Section from "./Section";
 import TripCard from "./TripCard";
+import MainButton from "./MainButton";
 import { previousTrips } from "@/data/previousTrips";
+import { useRouter } from "next/navigation";
 
 function Prev() {
+  const route = useRouter();
   return (
     <>
       <Section
@@ -11,10 +14,15 @@ function Prev() {
         sectionHeading="Previous Trips"
         headingStyle="text-brown"
       >
-        <div className="w-fit mx-auto grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8 pt-4 pb-16 place-items-center">
+        <div className="w-fit mx-auto grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8 pt-4 pb-6 place-items-center">
           {previousTrips.slice(0, 6).map((trip, idx) => (
             <TripCard key={idx} trip={trip} />
           ))}
+        </div>
+        <div className="flex items-center justify-center mb-16">
+          <MainButton onClick={() => route.push("/previous-trips")}>
+            View More
+          </MainButton>
         </div>
       </Section>
     </>
