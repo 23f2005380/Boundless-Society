e49233fcb05233fdf435b8d8f8a70e31f385c4fa
rahul <rahulbarakoti4@gmail.com>
2025-07-04 21:24:58 +0530

    gallery images updated

--- framer-welcome/components/MainButton.jsx
+++ framer-welcome/components/MainButton.jsx
@@ -4,7 +4,7 @@ const MainButton = ({ children, onClick }) => {
   return (
     <button
       onClick={onClick}
-      className="px-6 font-oswald font-bold py-2 rounded-3xl border-0 bg-[#ffe878] text-black text-[20px] tracking-wider 
+      className="px-6 font-oswald font-bold py-2 rounded-2xl border-0 bg-[#ffe878] text-black text-[20px] tracking-wider 
                  shadow-[0_8px_0_0_rgb(59,0,27)] transition-all duration-300 ease-in-out 
                  hover:shadow-[0_3px_0_0_rgb(59,0,27)] active:bg-[#ffe878] active:shadow-none hover:translate-y-[4px] active:translate-y-[5px]"
     >
--- framer-welcome/data/gallery.js
+++ framer-welcome/data/gallery.js
@@ -1,11 +1,7 @@
 export const gallery = [
-  { name: "Goa Vibes", img: "/Gal1.jpg", link: "#" },
-  { name: "Goa Vibes", img: "/Gal1.jpg", link: "#" },
-  { name: "Goa Vibes", img: "/Gal1.jpg", link: "#" },
-  { name: "Goa Vibes", img: "/Gal1.jpg", link: "#" },
-  { name: "Goa Vibes", img: "/Gal1.jpg", link: "#" },
-  { name: "Goa Vibes", img: "/Gal1.jpg", link: "#" },
-  { name: "Goa Vibes", img: "/Gal1.jpg", link: "#" },
-  { name: "Goa Vibes", img: "/Gal1.jpg", link: "#" },
-  { name: "Goa Vibes", img: "/Gal1.jpg", link: "#" },
+  { name: "Goa Vibes", img: "/images/Gallery/Gal1.png", link: "#" },
+  { name: "Darjeeling Trip", img: "/images/Gallery/Gal2.png", link: "#" },
+  { name: "Ooty Trip", img: "/images/Gallery/Gal3.png", link: "#" },
+  { name: "Chopta Trip", img: "/images/Gallery/Gal4.png", link: "#" },
+  { name: "Lonavala Trip", img: "/images/Gallery/Gal5.png", link: "#" },
 ];
