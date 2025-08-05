90770b63faca8f1753e665751dfd99fed911f3b3
23f2005380 <23f2005380@ds.study.iitm.ac.in>
2025-08-05 13:35:01 +0530

    More photos

--- framer-welcome/components/New.tsx
+++ framer-welcome/components/New.tsx
@@ -7,14 +7,14 @@ const councilMembers = [
   { name: "Ishan Raj", role: "General Secretary", image: "/founders/ishan.jpg"}
 ];
 const departmentHeads = [
-  { name: "Aman Kumar", role: "Technical", image: "/founders/aman3.jpg" },
+  { name: "Aman Kumar", role: "Technical", image: "/founders/aman.jpg" },
 
   { name: "Nityashree", role: "Females Corner", image: "/founders/nitya.jpg" },
   { name: "Vishnu M.", role: "Public Relations", image: "/founders/vishnu.jpg" },
   { name: "Saurav Pal", role: "Research", image: "/founders/saurav.jpg" },
 
   { name: "Vivek Subramani", role: "Media", image: "/founders/vivek.jpg" },
-  { name: "Alok Chaubey", role: "Documentation and Graphic Designing", image: "/founders/kanika.jpg" },
+  { name: "Alok Chaubey", role: "Documentation and Graphic Designing", image: "/founders/alok.jpg" },
   { name: "Ananay Purvey", role: "City Operation", image: "/founders/ananay.jpg" },
     { name: "Ankaj Kumar", role: "Trip Coordination", image: "/founders/ankaj.jpg" },
       { name: "Satyam Pandey", role: "Trip Coordination", image: "/founders/satyam.jpg" },
