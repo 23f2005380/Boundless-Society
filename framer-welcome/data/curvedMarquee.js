85d23d060d1a25641b95f1f1a127167de402d6fc
MK-PANKAJ <manishkumarpankaj119@gmail.com>
2025-10-21 17:31:11 +0000

    Shimoga spelling and stats update

--- framer-welcome/data/curvedMarquee.js
+++ framer-welcome/data/curvedMarquee.js
@@ -1,7 +1,7 @@
 export const curvedMarque = [
   { title: "Vrindavan", img: "/Proud/Agra.jpg" },
   { title: "Mewar", img: "/Proud/Mewar.webp" },
-  { title: "Simguna", img: "/Proud/Simguna.jpg" },
+  { title: "Shimoga", img: "/Proud/Simguna.jpg" },
   { title: "Goa", img: "/Proud/Goa.jpg" },
   { title: "Darjeeling", img: "/Proud/Darjeeling.jpg" },
   { title: "Ooty", img: "/Proud/Ooty.jpg" },
--- framer-welcome/data/gallery.js
+++ framer-welcome/data/gallery.js
@@ -2,7 +2,7 @@ export const gallery = [
   { name: "Goa Vibes", img: "/images/Gallery/Gal1.png", link: "#" },
   { name: "Darjeeling Trip", img: "/images/Gallery/Gal2.png", link: "#" },
   { name: "Ooty Trip", img: "/images/Gallery/Gal3.png", link: "#" },
-  { name: "Simguna Trip", img: "/images/Gallery/Gal7.jpg", link: "#" },
+  { name: "Shimoga Trip", img: "/images/Gallery/Gal7.jpg", link: "#" },
   { name: "Chopta Trip", img: "/images/Gallery/Gal4.png", link: "#" },
   { name: "Lonavala Trip", img: "/images/Gallery/Gal5.png", link: "#" },
   { name: "Mewar Trip", img: "/images/Gallery/Gal6.jpg", link: "#" },
