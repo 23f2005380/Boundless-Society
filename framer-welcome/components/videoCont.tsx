f6d7fc2b16e8634b96a15fb5346f188050fd8e10
rahul <rahulbarakoti4@gmail.com>
2025-11-03 22:09:58 +0530

    video updated

--- framer-welcome/components/videoCont.tsx
+++ framer-welcome/components/videoCont.tsx
@@ -1,102 +1,100 @@
 import React, { useEffect, useRef } from "react";
 
 export default function VideoContainer() {
-    const videoRef = useRef<HTMLIFrameElement>(null);
+  const videoRef = useRef<HTMLIFrameElement>(null);
 
-    useEffect(() => {
-        const timer = setTimeout(() => {
-            if (videoRef.current) {
-                // Post message to the YouTube iframe to play the video
-                videoRef.current.contentWindow?.postMessage(
-                    JSON.stringify({
-                        event: "command",
-                        func: "playVideo",
-                        args: [],
-                    }),
-                    "*"
-                );
-            }
-        }, 1000);
-        return () => clearTimeout(timer);
-    }, []);
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      if (videoRef.current) {
+        // Post message to the YouTube iframe to play the video
+        videoRef.current.contentWindow?.postMessage(
+          JSON.stringify({
+            event: "command",
+            func: "playVideo",
+            args: [],
+          }),
+          "*"
+        );
+      }
+    }, 1000);
+    return () => clearTimeout(timer);
+  }, []);
 
-    const circles = [];
-    for (let i = 15; i > 5; i--) {
-        const size = i * 10;
-        if (i === 6) {
-            circles.push(
-                <div
-                    key={i}
-                    style={{
-                        position: "absolute",
-                        top: "50%",
-                        left: "50%",
-                        width: `${size}vw`,
-                        height: `${size}vw`,
-                        transform: "translate(-50%, -50%)",
-                        borderRadius: "50%",
-                        background: "#fffae9",
-                        boxShadow: "0 4px 24px rgba(84,63,63,1)",
-                        display: "flex",
-                        alignItems: "center",
-                        justifyContent: "center",
-                        overflow: "hidden",
-                    }}
-                >
-                    <iframe
-                        ref={videoRef}
-                        width="100%"
-                        height="100%"
-                       
-                        src="https://www.youtube.com/embed/XwLwUz69z3U?autoplay=1&controls=0&loop=10&mute=1&modestbranding=1&showinfo=0&rel=0"
-                        title="YouTube video player"
-                        frameBorder="0"
-                        allow="autoplay; encrypted-media"
-                        allowFullScreen
-                        
-                        style={{
-                            borderRadius: "50%",
-                            width: "100%",
-                            height: "100%",
-                            objectFit: "cover",
-                        }}
-                    ></iframe>
-                </div>
-            );
-        } else {
-            circles.push(
-                <div
-                    key={i}
-                    style={{
-                        position: "absolute",
-                        top: "50%",
-                        left: "50%",
-                        width: `${size}vw`,
-                        height: `${size}vw`,
-                        transform: "translate(-50%, -50%)",
-                        borderRadius: "50%",
-                        background: "#fffae9",
-                        boxShadow: "0 4px 24px rgba(84,63,63,1)",
-                    }}
-                />
-            );
-        }
-    }
-
-    return (
+  const circles = [];
+  for (let i = 15; i > 5; i--) {
+    const size = i * 10;
+    if (i === 6) {
+      circles.push(
         <div
-            style={{
-                position: "relative",
-                width: "100vw",
-                maxWidth: "100vw",
-                height: "100vw",
-                maxHeight: "100vh",
-                margin: "0 auto",
-                overflow: "hidden",
-            }}
+          key={i}
+          style={{
+            position: "absolute",
+            top: "50%",
+            left: "50%",
+            width: `${size}vw`,
+            height: `${size}vw`,
+            transform: "translate(-50%, -50%)",
+            borderRadius: "50%",
+            background: "#fffae9",
+            boxShadow: "0 4px 24px rgba(84,63,63,1)",
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "center",
+            overflow: "hidden",
+          }}
         >
-            {circles}
-            <style>{`
+          <iframe
+            ref={videoRef}
+            width="100%"
+            height="100%"
+            src="https://www.youtube.com/embed/et-Th2dwGVA?autoplay=1&controls=0&loop=10&mute=1&modestbranding=1&showinfo=0&rel=0"
+            title="YouTube video player"
+            frameBorder="0"
+            allow="autoplay; encrypted-media"
+            allowFullScreen
+            style={{
+              borderRadius: "50%",
+              width: "100%",
+              height: "100%",
+              objectFit: "cover",
+            }}
+          ></iframe>
+        </div>
+      );
+    } else {
+      circles.push(
+        <div
+          key={i}
+          style={{
+            position: "absolute",
+            top: "50%",
+            left: "50%",
+            width: `${size}vw`,
+            height: `${size}vw`,
+            transform: "translate(-50%, -50%)",
+            borderRadius: "50%",
+            background: "#fffae9",
+            boxShadow: "0 4px 24px rgba(84,63,63,1)",
+          }}
+        />
+      );
+    }
+  }
+
+  return (
+    <div
+      style={{
+        position: "relative",
+        width: "100vw",
+        maxWidth: "100vw",
+        height: "100vw",
+        maxHeight: "100vh",
+        margin: "0 auto",
+        overflow: "hidden",
+      }}
+    >
+      {circles}
+      <style>{`
                 @media (min-width: 768px) {
                     div[style*="position: relative"] {
                         width: 70vw !important;
@@ -106,6 +104,6 @@ export default function VideoContainer() {
                     }
                 }
             `}</style>
-        </div>
-    );
+    </div>
+  );
 }
