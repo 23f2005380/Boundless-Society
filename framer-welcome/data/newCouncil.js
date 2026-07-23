0ffca514b3653465cf8ff94f829d38f98d383677
Navjot Singh Dadiala <23f2000835@ds.study.iitm.ac.in>
2026-07-23 23:41:08 +0530

    females co change

--- framer-welcome/data/newCouncil.js
+++ framer-welcome/data/newCouncil.js
@@ -4,7 +4,7 @@ export const councilMembers = [
 ];
 export const departmentHeads = [
     { name: "Aman Kumar", role: "Technical", image: "https://res.cloudinary.com/duuyaejwy/image/upload/v1761071427/aman_efh83y.jpg" },
-    { name: "Nityashree", role: "Females Corner", image: "https://res.cloudinary.com/duuyaejwy/image/upload/v1761071437/nitya_mye2zd.jpg" },
+    { name: "Kamayani Mishra", role: "Females Corner", image: "https://res.cloudinary.com/dia2m9kdb/image/upload/v1784830149/kama_nmtxvo.jpg" },
     { name: "Akanksha Rath", role: "Females Corner", image: "https://res.cloudinary.com/dia2m9kdb/image/upload/v1775897401/WhatsApp_Image_2026-04-11_at_14.17.09_dneu77.jpg" },
     { name: "Vishnu M.", role: "Public Relations", image: "https://res.cloudinary.com/duuyaejwy/image/upload/v1761071444/vishnu_hq0zyv.jpg" },
     { name: "Saurav Pal", role: "Research", image: "https://res.cloudinary.com/duuyaejwy/image/upload/v1761071440/saurav_sbude7.jpg" },
@@ -13,4 +13,4 @@ export const departmentHeads = [
     { name: "Ananay Purvey", role: "City Operation", image: "https://res.cloudinary.com/dia2m9kdb/image/upload/v1776792765/WhatsApp_Image_2026-04-21_at_22.45.30_vy7ifa.jpg" },
     { name: "Ankaj Kumar", role: "Trip Coordination", image: "https://res.cloudinary.com/duuyaejwy/image/upload/v1761071430/ankaj_cvinmw.jpg" },
     { name: "Satyam Pandey", role: "Trip Coordination", image: "https://res.cloudinary.com/duuyaejwy/image/upload/v1761071440/satyam_mzkvao.jpg" },
-];
+]; 
